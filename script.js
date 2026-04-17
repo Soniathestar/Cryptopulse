d997a986130bfac15d17fb8d8f03f0a3d7290a38
Sonia <soniangaruiya@gmail.com>
2026-04-17 11:06:41 +0300

    Grab elements from HTML using DOM manipulation

---
+++ script.js
@@ -0,0 +1,6 @@
+//DOM manipulation
+const convertBtn = document.getElementById("converter");
+const amountInput = document.getElementById("amount");
+const currencySelect = document.getElementById("popcurrencies");
+const resultEl = document.getElementById("results");
+const searchInputCoin = document.getElementById("searchtext");
