07d7fffd13cd55a3c3c781d6733e37018c8683e7
Sonia <soniangaruiya@gmail.com>
2026-04-17 11:16:57 +0300

    Get data from coinGecko API and validate the data

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ convertBtn.addEventListener("click", async  () => {
     resultEl.textContent = "Enter a valid amount and crypto name first.";
     return;
      }
+     //Fetching data from the coinGecko API
+    try {
+        const res = await fetch(
+            `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${currency}`
+        );
+        const data = await res.json();
+
+        if (!data[coin]) {
+            resultEl.textContent = "Invalid crypto name.";
+            return;
+        }
+    }
